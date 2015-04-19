17185a5353a24321fb7f94bcd4c24835dc5cd42c
Nikola Jovanovic <koorweetzaah@gmail.com>
2015-04-19 16:50:05 +0200

    Update Graham Scan.cpp

--- Geometry/Graham Scan.cpp
+++ Geometry/Graham Scan.cpp
@@ -1,114 +1,117 @@
-//RandomUsername(Nikola Jovanovic)
-//Graham Scan(Convex Hull) (nlogn)
-#include <cstdlib>
-#include <iostream>
-#include <stdio.h>
-#include <vector>
-#include <math.h>
-#include <algorithm>
-#define MAXN 10000
+// RandomUsername(Nikola Jovanovic)
+// Graham Scan (Convex Hull)
+// O(nlogn)
+
+#include <bits/stdc++.h>
+#define MAXN 100005
+#define lld long long
+#define ff(i,a,b) for(int i=a; i<=b; i++)
+#define fb(i,a,b) for(int i=a; i>=b; i--)
+#define par pair<int, int>
+#define fi first
+#define se second
+#define mid (l+r)/2
 
 using namespace std;
 
-const double PI = 3.1415926;
+/*
+TP (http://oi59.tinypic.com/35b86pz.jpg):
+
+IN:
+15
+-4 2    2 -4    4 -3    6 -2    -2 -1   3 2     -3 -4
+4 3     2 6     5 3     4 6     -1 6    -1 2    0 -4    0 5
+
+OUT:
+7 14 2 3 4 10 11 9 12 1
+*/
+
+const double PI = 3.1415926535;
 
 struct point
 {
-       int num;//all the points are numbered (input)
-       int x,y; //coordinates
-       double ang;//the angle this point and the starting point make with the x-axis
+       double x, y;
+       int idx;
+       double ang; //the angle this point and the starting point make with the x-axis
+       double qdist; //distance^2 from the starting point, for dealing with ang ties
+       const bool operator < (const point b)
+       const
+       {
+           if(ang == b.ang)
+            return qdist < b.qdist;
+           return ang < b.ang;
+       }
 };
 
-int n;//number of points
-point points[MAXN];//all the points are stored here
-int p,py,px;
-vector<point> hull;//the hull is stored here
-int hullsize;
-
-bool cmp(point a,point b)//compare two points by their angles
-{
-     return a.ang<b.ang;
-}
+int n; //number of points
+point a[MAXN]; //all the points are stored here
+point p; //starting point
+vector<point> hull; //the hull is stored here
 
 int turn(point p1, point p2, point p3)
 {
     return (p2.x - p1.x) * (p3.y - p1.y) - (p2.y - p1.y) * (p3.x - p1.x);
-    //cross product based
-    //returns <0 if right turn, >0 if left turn, =0 when colinear
+    // cross product
+    // <0 - right turn
+    // >0 - left turn
+    // =0 - collinear
 }
 
-void Graham()//The algorithm
+void GrahamScan()
 {
-    //We found the starting point (during the input), now calculate the angles
-    //every point and the starting point make with the x-axis
-    double ylen,xlen;//lengths of sides
-    for(int i=0;i<=n-1;i++)
-     {  
-         ylen=(double)(points[i].y-points[p].y);
-         xlen=(double)(points[i].x-points[p].x);
-         points[i].ang=atan2(ylen,xlen);//atan 2: [-PI,PI]. p=miny => [0,PI]
-     }
-    sort(points,points+n,cmp);//Do the harlem shake!
-    //First two points must be in the hull, so add them to the vector and increase hullsize
-    hull.push_back(points[0]);
-    hull.push_back(points[1]);
-    hullsize=2;
-    point a=hull[0],b=hull[1],curr;//deep copies? yep! c# would fail here!
-    //Do the scan:
-    for(int i=2;i<=n-1;i++)//scan all the points
+    hull.clear();
+
+    //Starting point
+    p = a[1];
+    for(int i=2; i<=n; i++)
     {
-            //a and b is the last fixed pair and we are now inspecting curr
-            curr=points[i];
-            while( turn(a,b,curr)>0 )//while not left turn or colinear:
-            {                        //(colinears are taken cuz we need all the points on c.h.)
-                   hull.pop_back();  //rollback!
-                   hullsize--;       //remove the middle point, and try to match the others
-                   a = hull[hullsize - 2];//second to last valid point
-                   b = hull[hullsize - 1];//last valid point
-            }
-            hull.push_back(curr);//when curr is finaly satisfying the conditions
-            hullsize++;          //add it to the hull and increase currsize
-            a=b;                 //also, update the triplet 
-            b=curr;              //we're observing
+        if( a[i].y < p.y || (a[i].y == p.y && a[i].x < p.x) )
+            p = a[i];
+    }
+
+    //Calculate angles and sort
+    for(int i=1; i<=n; i++)
+    {
+        //atan2: [-PI,PI]. p.y = miny => [0,PI]
+        a[i].ang = atan2(a[i].y - p.y, a[i].x - p.x);
+        a[i].qdist = (a[i].y - p.y) * (a[i].y - p.y) + (a[i].x - p.x) * (a[i].x - p.x);
+    }
+    sort(a+1, a+n+1);
+
+    //Start the scan
+
+    // intially, first two points are in the hull
+    hull.push_back( a[1] );
+    hull.push_back( a[2] );
+
+    for(int i=3; i<=n; i++) //scan all the points
+    {
+        // we observe the turn last two points make with the point we're trying to introduce
+        // while it's a right turn we discard the last hull point
+        // we keep discarding until we can safely add a[i]
+        // < - all points on the hull
+        // <= - the least number of points on the hull (no 3 collinear)
+
+        while( turn( hull[hull.size() - 2], hull[hull.size() - 1], a[i]) < 0 )
+            hull.pop_back();
+        hull.push_back(a[i]);
     }
 }
 
 int main()
 {
-    scanf("%d",&n);
-    scanf("%d %d",&points[0].x,&points[0].y);//numbered from 0, the dots are. #yoda
-    points[0].num=0;
-    p=0, py=points[0].y, px=points[0].y;       //detecting a starting point
-    for(int i=1;i<=n-1;i++)
+    scanf("%d", &n);
+    for(int i=1; i<=n; i++)
     {
-            scanf("%d %d",&points[i].x,&points[i].y);
-            points[i].num=i;
-            if(points[i].y<py || (points[i].y==py && points[i].x<px))//update starting point
-                p=points[i].num, py=points[i].y, px=points[i].y;
+        scanf("%lf %lf", &a[i].x, &a[i].y);
+        a[i].idx = i;
     }
-    Graham();//Do the scan
-    //Print the hull:
+    GrahamScan();
+
+    //Output:
     printf("Convex hull:\n");
-    for(int i=0;i<=hullsize-1;i++)
-      printf("%d ",hull[i].num);
-    printf("\n");
-    
-    system("PAUSE");
+    for(int i=0; i<hull.size(); i++)
+      printf("%d(%f, %f) ", hull[i].idx, hull[i].x, hull[i].y);
+
     return 0;
 }
-/*
-Test case:
-10
-1 -1
-0 2
--3 -3
-5 -2
-4 1
-6 3
-2 5
-1 -1
--3 5
--4 0
-http://upload.wikimedia.org/wikipedia/commons/d/de/ConvexHull.svg
-Stupid numbering, ignore it.
-*/
